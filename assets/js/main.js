1ebbd9d7228601736dc9570a4986e1cc207d4a58
Letícia Lopes <leticiaflopes@outlook.com>
2023-04-06 20:00:14 -0300

    adicionado mudanças

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+var elementMaceio = document.querySelector('#travel-maceio');
+
+elementMaceio.addEventListener('click', openDetailMaceio);
+
+function openDetailMaceio() {
+    window.open('details-maceio.html');
+}
